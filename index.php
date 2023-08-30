84f06fc23aa157e3414c1a92bbf9dbcc3c77e570
Eli Jean <97365554+Ejad1@users.noreply.github.com>
2023-08-30 21:47:56 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>QR Code Generator</title>
+
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>QR Code Generator</h1>
-    <form action="" method="post">
-        <label for="data">Texte :</label>
-        <input type="text" name="data" required>
-        <button type="submit">Générer QR Code</button>
-    </form>
+    <div class="en_tete">
+        <h1>QR Code Generator</h1>
+        <form action="" method="post">
+            <label for="data">Texte :</label>
+            <input type="text" name="data" required>
+            <button type="submit">Générer QR Code</button>
+        </form>
+    </div>
 
     <?php
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -27,6 +34,6 @@
             echo '<p>Veuillez entrer un texte pour générer le code QR.</p>';
         }
     }
-    ?>
+    ?>    
 </body>
 </html>
